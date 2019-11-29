211f061de569a98870524324952465db901608c1
Johann <johanson88@gmail.com>
2019-11-29 11:47:45 +0100

    get cocktailbyid OK

--- scripts/cocktail-card.js
+++ scripts/cocktail-card.js
@@ -1,3 +1,18 @@
 
-    const cocktailId = localStorage.getItem('cocktail-id');
-    console.log(cocktailId);
+const cocktailId = localStorage.getItem('cocktail-id').slice(9);
+console.log(cocktailId);
+const baseUrl = 'https://www.thecocktaildb.com/api/json/v1/1/';
+
+const lookup = 'lookup.php';
+
+
+
+const getCocktailById = (id) => {
+    axios.get(baseUrl + lookup + '?i=' + id)
+        .then ( responseFromAPI => {
+            console.log(responseFromAPI);
+        })
+}
+
+getCocktailById(cocktailId);
+
